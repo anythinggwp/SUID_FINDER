0b5d33f08e6c92defc271a925875f1ff735005a7
anythinggwp <OrangeKotE@yandex.ru>
2025-12-14 13:53:54 +0300

    added crt-bridge script

--- Makefile
+++ Makefile
@@ -1,13 +1,2 @@
 create-bridge:
-	sudo ip link add br0 type bridge
-	sudo ip link set br0 up 
-	sudo ip link set enp1s0 down
-	sudo ip link set enp1s0 master br0
-
-	@while ! bridge link | grep -q "enp1s0.*master br0"; do sleep 0.1; done
-	
-	sudo ip addr flush dev enp1s0
-	sudo ip addr add 192.168.100.202/24 dev br0
-	sudo ip route add default via 192.168.100.1
-	sudo ip link set enp1s0 up
-	echo -e "nameserver 192.168.100.1\nnameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+	sudo ./cmd/create-bridge.sh
---
+++ cmd/create-bridge.sh
@@ -0,0 +1,28 @@
+# !/bin/sh
+DNS_TO_ADD="8.8.8.8"
+RESOLV="/etc/resolv.conf"
+
+ip link add br0 type bridge
+ip link set br0 up
+ip link set enp1s0 down
+ip link set enp1s0 master br0
+ip addr flush dev enp1s0
+
+while true; do
+    # проверяем есть ли IP у enp1s0
+    IP=$(ip -br a | awk '/enp1s0/ {print $3}')
+    
+    if [ "$IP" = "" ]; then
+        # если IP нет, добавляем DNS, если его ещё нет
+        if ! grep -q "$DNS_TO_ADD" "$RESOLV"; then
+            echo "Добавляем DNS $DNS_TO_ADD в $RESOLV"
+            echo "nameserver $DNS_TO_ADD" | sudo tee -a "$RESOLV"
+        fi
+    fi
+
+    sleep 1
+done
+ip addr add 192.168.100.202/24 dev br0
+ip route add default via 192.168.100.1
+ip link set enp1s0 up
+echo -e "nameserver 192.168.100.1\nnameserver 8.8.8.8" | tee /etc/resolv.conf
