2cf7d39f40ca30acb34ce5e9bdba2f8f62c2ff34
anythinggwp <OrangeKotE@yandex.ru>
2025-12-21 22:50:15 +0300

    ya ne mogy blyat da skolko mojno

--- cmd/gcc_build_script.sh
+++ cmd/gcc_build_script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -e
+
 TS=$(date +%Y%m%d_%H%M%S)
 
 mkdir -p ~/logs/
 
 cd $1 || exit 1
-
+echo "Current path $1"
 echo "Запускаем сборку $2"
-time -f "Real time %e сек  (user: %U сек, sys: %S сек)" make -j$(nproc) 2&> ~/logs/"$2-$TS.log"
+/usr/bin/time -f "Real time %e сек  (user: %U сек, sys: %S сек)" make -j$(nproc) 2> ~/logs/"$2-$TS.log"
 # time -f "Real time %e сек  (user: %U сек, sys: %S сек)" apt-get update 2&> ~/logs/"$2-$TS.log"
