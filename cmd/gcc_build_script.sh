0fa7ae296c0535eff517956bea49223b6f42074a
anythinggwp <OrangeKotE@yandex.ru>
2025-12-21 22:56:11 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- cmd/gcc_build_script.sh
+++ cmd/gcc_build_script.sh
@@ -2,11 +2,11 @@
 set -e
 
 TS=$(date +%Y%m%d_%H%M%S)
-
+LOG=~/logs/"$2-$TS.log"
 mkdir -p ~/logs/
 
 cd $1 || exit 1
 echo "Current path $1"
 echo "Запускаем сборку $2"
-/usr/bin/time -f "Real time %e сек  (user: %U сек, sys: %S сек)" make -j$(nproc) 2> ~/logs/"$2-$TS.log"
+/usr/bin/time -o "$LOG" -f "Real time %e сек  (user: %U сек, sys: %S сек)" make -j$(nproc)
 # time -f "Real time %e сек  (user: %U сек, sys: %S сек)" apt-get update 2&> ~/logs/"$2-$TS.log"
