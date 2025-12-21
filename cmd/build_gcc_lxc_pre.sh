777126cf0c9be4256abd5e9f699ceb0d3da77786
anythinggwp <OrangeKotE@yandex.ru>
2025-12-22 00:42:43 +0300

    added lxc start script

--- cmd/build_gcc_lxc_pre.sh
+++ cmd/build_gcc_lxc_pre.sh
@@ -5,56 +5,58 @@ CONTNAME1="cont-1"
 CONTNAME2="cont-2"
 CONTNAME3="cont-3"
 GCCSOURCE="$HOME/git"
-BUILD1="/tmp/gcc-tmp-tty1/"
-BUILD2="/tmp/gcc-tmp-tty2/"
-BUILD3="/tmp/gcc-tmp-tty3/"
-RESULT1="/tmp/gcc-tty1/"
-RESULT2="/tmp/gcc-tty2/"
-RESULT3="/tmp/gcc-tty3/"
+BUILD="/test/gcc-build"
+LOGS="/test/logs"
 GCCREPO="https://github.com/gcc-mirror/gcc"
 
 echo "Создаем контейнер"
-sudo lxc-create -t astralinux-se -n "$CONTNAME1" 
-sudo lxc-start -n "$CONTNAME1" 
+sudo lxc-create -t astralinux-se -n "$CONTNAME1"
+sudo lxc-start -n "$CONTNAME1"
 
 run_in_container() {
     local name="$1"
     local cmd="$2"
-    sudo lxc-attach -n "$CONTAINER_NAME" -- bash -c "$cmd" 
+    sudo lxc-attach -n "$CONTAINER_NAME" -- bash -c "$cmd"
 }
 
 echo "Обновляем и устанавливаем зависимости в контейнер"
-run_in_container "$CONTNAME1" "apt-get update && apt-get install -y build-essential gcc g++ make flex bison libgmp-dev libmpfr-dev libmpc-dev texinfo wget git"
-sudo lxc-stop -n "$CONTNAME1" 
+run_in_container "$CONTNAME1" "apt-get update && apt-get install -y build-essential gcc g++ make flex bison libgmp-dev libmpfr-dev libmpc-dev texinfo wget git time"
+run_in_container "$CONTNAME1" "mkdir -p $BUILD $LOGS /opt/gcc-test-build"
+sudo lxc-stop -n "$CONTNAME1"
+
+echo "Копируем исходники gcc в контейнер"
 sudo rsync -av ~/git/gcc/ /var/lib/lxc/cont-1/rootfs/root/gcc/
+sudo lxc-start -n "$CONTNAME1"
 
-# echo "Клонируем GCC (если еще не склонирован)..."
-# run_in_container "mkdir -p $SRC_DIR && cd $SRC_DIR && if [ ! -d gcc ]; then git clone $GCC_REPO; fi"
+echo "Запускаем конфигурирование сборки"
 
-# echo "Создаем папку сборки..."
-# run_in_container "rm -rf $BUILD_DIR && mkdir -p $BUILD_DIR"
+run_in_container "$CONTNAME1" "
+cd $BUILD
+/root/gcc/configure \
+    --prefix=/opt/gcc-test-build \
+    --enable-languages=c \
+    --disable-multilib \
+    --disable-bootstrap \
+    --disable-libstdcxx \
+    --disable-libquadmath \
+    --disable-libsanitizer \
+    --disable-libssp \
+    --disable-libgomp \
+    --disable-libatomic \
+    --disable-nls
+"
+sudo lxc-stop -n "$CONTNAME1"
 
-# echo "Конфигурируем сборку..."
-# run_in_container "
-# cd $BUILD_DIR
-# $SRC_DIR/gcc/configure \
-#     --prefix=$PREFIX_DIR \
-#     --enable-languages=c \
-#     --disable-multilib \
-#     --disable-bootstrap \
-#     --disable-libstdcxx \
-#     --disable-libquadmath \
-#     --disable-libsanitizer \
-#     --disable-libssp \
-#     --disable-libgomp \
-#     --disable-libatomic \
-#     --disable-nls
-# "
+echo "Клонируем контейнеры"
+sudo lxc-clone -n "$CONTNAME1" -N "$CONTNAME2"
+sudo lxc-clone -n "$CONTNAME1" -N "$CONTNAME3"
 
-# echo "Запускаем сборку GCC..."
-# run_in_container "cd $BUILD_DIR && make -j\$(nproc)"
+# echo "Запускаем контейнеры"
+# sudo lxc-start -n "$CONTNAME1"
+# sudo lxc-start -n "$CONTNAME2"
+# sudo lxc-start -n "$CONTNAME3"
 
-# echo "Установка GCC..."
-# run_in_container "cd $BUILD_DIR && make install"
+# run_in_container_parallel "$CONTNAME1" "cd $BUILD /usr/bin/time -o $LOGS -f 'Real time %e сек  (user: %U сек, sys: %S сек)' make -j$(nproc)"
+# run_in_container_parallel "$CONTNAME2" "cd $BUILD /usr/bin/time -o $LOGS -f 'Real time %e сек  (user: %U сек, sys: %S сек)' make -j$(nproc)"
+# run_in_container_parallel "$CONTNAME3" "cd $BUILD /usr/bin/time -o $LOGS -f 'Real time %e сек  (user: %U сек, sys: %S сек)' make -j$(nproc)"
 
-# echo "Сборка завершена. GCC установлен в $PREFIX_DIR внутри контейнера $CONTAINER_NAME"
--- cmd/build_gcc_lxc_start.sh
+++ cmd/build_gcc_lxc_start.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-echo "жопа"
-sleep 60
+CONTNAME1="cont-1"
+CONTNAME2="cont-2"
+CONTNAME3="cont-3"
+BUILD="/test/gcc-build"
+LOGS="/test/logs"
+
+run_in_container_parallel() {
+    local name="$1"
+    local cmd="$2"
+    fly-term -e sudo lxc-attach -n "$CONTAINER_NAME" -- bash -c "$cmd" &
+}
+
+run_in_container_parallel "$CONTNAME1" "cd $BUILD /usr/bin/time -o $LOGS -f 'Real time %e сек  (user: %U сек, sys: %S сек)' make -j$(nproc)"
+run_in_container_parallel "$CONTNAME2" "cd $BUILD /usr/bin/time -o $LOGS -f 'Real time %e сек  (user: %U сек, sys: %S сек)' make -j$(nproc)"
+run_in_container_parallel "$CONTNAME3" "cd $BUILD /usr/bin/time -o $LOGS -f 'Real time %e сек  (user: %U сек, sys: %S сек)' make -j$(nproc)"
